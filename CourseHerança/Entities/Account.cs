a6ee8c93f6298af92cd7b33157738172bbe2cafa
Davids-dev <davisson.assuncao@gmail.com>
2021-04-10 20:44:16 -0300

    Primeiras interações com o conceito de Herança

---
+++ CourseHerança/Entities/Account.cs
@@ -0,0 +1,31 @@
+﻿namespace CourseHerança.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; } //permite somente uma subclasse alterar o seu valor.
+
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void WithDraw(double amount) //recebendo uma quantia como parametro
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ CourseHerança/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CourseHerança.Entities;
+
+namespace CourseHerança
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+        }
+    }
+}
